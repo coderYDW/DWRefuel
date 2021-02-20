6dac3de52045499af08919f4ee9ff2b1e9b917c2
杨冬武 <coderYDW@users.noreply.github.com>
2021-02-20 11:32:08 +0800

    pod

--- Refuel/Refuel/ViewController.swift
+++ Refuel/Refuel/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    lazy var tableView: UITableView = {
+        let t = UITableView()
+        return t
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Hello World"
+        view.addSubview(label)
+        label.frame = CGRect(x: 100, y: 200, width: 300, height: 40)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+//        tableView.frame = view.bounds
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
     }
 
+    
+    // MARK: - tableView dataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = "第\(indexPath.section)组"
+        cell?.detailTextLabel?.text = "第\(indexPath.row)行"
+        return cell!
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+    
 }
 
